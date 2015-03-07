bd386e6078f7e3ba545f8c47e4816c6e02a0410c
Connor Osborn <cdosborn@uw.edu>
2015-03-07 02:50:40 -0700

    more dupr

--- scripts/basic.js
+++ scripts/basic.js
@@ -2,7 +2,7 @@ var repository = 'lit';
 var owner = 'cdosborn';
 var base = 'https://api.github.com/repos';
 var branch = 'gh-pages'; // or 'master'
-var reqUrl = base '/' + owner + '/' + repository + '/commits?sha=' + branch;
+var reqUrl = base + '/' + owner + '/' + repository + '/commits?sha=' + branch;
 
 var commits, head;
 
@@ -11,14 +11,33 @@ var App = function() {
     get(reqUrl, function() {
         commits = JSON.parse(this.responseText);
         head = commits[0]; 
-        App.checkout(head.sha, 'index.html', function(url) {
+        App.checkout(head.sha, 'index.html', function(content) {
+            // https://developer.github.com/v3/media/
+            var html = atob(content);
             var iframe = document.querySelector('iframe');
-            iframe.src = url;
+            iframe.srcdoc = html;
+            iframe.srcdoc = html.replace(/src="(.*?)"/g, function(match, path) {
+                App.correctPath(head.sha, path, function(url) {
+                });
+            });
+
         });
     });
 };
 
 App.checkout = function(hash, path, cb) {
+    //GET /repos/:owner/:repo/contents/:path?ref=hash
+
+    var reqUrl = base + '/' + owner + '/' + repository + '/contents/' + path + '?ref=' + hash
+    var file;
+    get(reqUrl, function() {
+        var json = JSON.parse(this.responseText);
+        //cb(json.download_url);
+        cb(json.content);
+    });
+};
+
+App.correctPath = function(hash, path, cb) {
     //GET /repos/:owner/:repo/contents/:path?ref=hash
 
     var reqUrl = base + '/' + owner + '/' + repository + '/contents/' + path + '?ref=' + hash
