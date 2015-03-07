31852ce47acab5d930631785c8017bfcfa8714ce
Andre Mercer <amercer1@email.arizona.edu>
2015-03-07 00:19:25 -0700

    Test Commit for contingious integration 	modified:   .gitignore 	new file:   gulpfile.js 	modified:   index.html 	modified:   package.json

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    rename = require("gulp-rename");
+
+
+var filesToMove = [
+    './index.html',
+    './scripts/**'
+];
+
+// move files
+gulp.task("move", function(){
+    gulp.src(filesToMove, { base: './' })
+    .pipe(gulp.dest("/var/www/heh"));
+});
+
+gulp.task('default', ['move']);
