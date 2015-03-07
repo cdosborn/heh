2620b81e719acb2d2f9288716f880bd0986731b6
loukitzanna <loukianova@email.arizona.edu>
2015-03-07 11:42:13 -0700

    updated index

---
+++ scripts/timeline.js
@@ -0,0 +1,16 @@
+var repository = 'lit';
+var owner = 'cdosborn';
+var base = 'https://api.github.com/repos';
+var branch = 'gh-pages'; // or 'master'
+var reqUrl = base + '/' + owner + '/' + repository + '/commits?sha=' + branch;
+
+var AllCommits = []
+
+var get = function(url, onload) { // gets list of commits
+    var r = new XMLHttpRequest();
+    r.onload = function(json){
+        AllCommits = json
+    }
+    r.open('GET', url)
+    r.send();
+}
