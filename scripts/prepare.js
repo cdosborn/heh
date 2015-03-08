46179e0f075662f4328c765ddc94aa0388a1b892
Connor Osborn <cdosborn@uw.edu>
2015-03-07 20:25:16 -0700

    woo prepare

---
+++ scripts/prepare.js
@@ -0,0 +1,22 @@
+var onSubmit = function(owner, repo) {
+    // /users/:username
+    get('https://api.github.com/users/' + owner, function() {
+        if (this.responseText != '') {
+            get('https://api.github.com/repos/' + owner + '/' + repo + '/commits?sha=gh-pages', function() {
+                var json = JSON.parse(this.responseText);
+                if (this.responseText != '') {
+                    window.commit_list = json;
+                    window.commit_index = 0;
+                    window.owner = owner;
+                    window.repo = repo;;
+                }
+            })
+        }
+    });
+}
+
+var onLeftArrow = function() {
+
+}
+var onRightArrow = function() {}
+
